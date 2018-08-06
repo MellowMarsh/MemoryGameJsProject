9ddd81c7b63d4e282b72a610f70e00f85f1cc676
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2018-08-06 14:54:45 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,26 +2,16 @@ var tileImages = ['tile1.jpg', 'tile2.jpg', 'tile3.jpg', 'tile4.jpg', 'tile5.jpg
 var gameboard = document.getElementById("gameboard");
 var buttonmessage = document.getElementById("gamecontrol");
 var mytime = document.getElementById("mytime");
-var cardsflippedover = 0;
-var lastcardpicked = -1;
-var timer = '';
-var clearTimeout="";
-//var score=0;
-var seconds = 0;
-var mseconds = 0;
-var minutes = 0;
-var hours = 0;
-var t= 0;
-//move varialbe
-let moves=0;
-let counter= document.querySelector(".moves");
-//variables for star icons
-let stars= document.querySelectorAll(".fa-star");
-//star list
-let starsList=document.querySelector('.start li');
-
-let popup=document.querySelector(".modal")
-
+var cardsflippedover = 0
+    , lastcardpicked = -1
+    , timer = ''
+    , score= 0
+    , seconds = 0
+    , mseconds = 0
+    , minutes = 0
+    , hours = 0;
+    moves=0;
+    moves.innerHTML=moves;
 
 var solutionArray = tileImages.concat(tileImages);
 document.getElementById("gamecontrol").addEventListener("click", startGame);
@@ -30,10 +20,10 @@ startGame();
 
 function startGame() {
     clearInterval(timer);
-  timerX();
+    timerX();
     seconds = 0, mseconds = 0, minutes = 0, hours = 0;
     shuffleArray(solutionArray);
-    //score = 0;
+    score = 0;
     gameboard.innerHTML = "";
 
     for (var i = 0; i <= ((solutionArray.length) - 1); i++) {
@@ -51,11 +41,7 @@ function pickCard(a, b, c) {
             if (fliparray[0] == fliparray[1]) {
                 console.log('same');
                 pickagain();
-                //score++;
-                //if(tileImages.length >=score){
-                //  console.log('END GAME');{}
-                 stopTimer();
-                //}
+              score++;
             }
             else {
                 timer = setInterval(hideCard, 1000);
@@ -63,23 +49,22 @@ function pickCard(a, b, c) {
                 //messageText("NO MATCH");
             }
         }
-        lastcardpicked =b;
+        lastcardpicked = b;
     }
 }
 //count moves made a display them in the html
-//Add move
-function addMoves(){
+function addMoves(i){
     moves++;
-    counter.innerHTML = moves;
+    countmoves.innerHTML = moves;
     //start timer on first click
     if(moves == 1){
        seconds = 0;
       mseconds = 0;
        minutes = 0;
        hours = 0;
-      startTimer();
+        startTimer();
     }
-    // setting hide stars based on moves
+    // setting rates based on moves
     if (moves > 6 && moves < 12){
         for( var i= 0; i < 3; i++){
             if(i > 1){
@@ -94,8 +79,7 @@ function addMoves(){
             }
         }
     }
-  }
-
+}
 
 function addTime() {
     seconds++;
@@ -108,11 +92,11 @@ function addTime() {
         }
     }
     mytime.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-   timerX();
+    timerX();
 }
 
 function timerX() {
-t=setTimeout(addTime, 1000);
+    t = setTimeout(addTime, 1100);
 }
 
 function pickagain() {
@@ -133,18 +117,6 @@ function hideCard() {
     pickagain();
 }
 
-
-//using Bootstrap modal I want to open modal when last card is picked.  The modal needs to show total moves, star rate, and total time.
-
-function stopTimer(b){
-
-{
-    clearInterval(timer);
-    finalTime=mytime.innerHTML;
-    $('#myModal').modal('show');
-//$('#mymodal').modal('hide'):
-}
-}
 function shuffleArray(d) {
     for (var c = d.length - 1; c > 0; c--) {
         var b = Math.floor(Math.random() * (c + 1));
