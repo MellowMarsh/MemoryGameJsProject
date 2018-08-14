5792741c5349afd0c1267b52c0c9fe58ba3068ca
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2018-08-14 15:02:54 -0500

    updated modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var tileImages = ['tile1.jpg', 'tile2.jpg', 'tile3.jpg', 'tile4.jpg', 'tile5.jpg', 'tile6.jpg'];
 var gameboard = document.getElementById("gameboard");
-var button= document.getElementById("restart");
+var button = document.getElementById("restart");
 var mytime = document.getElementById("mytime");
 var cardsflippedover = 0;
 var lastcardpicked = -1;
@@ -13,15 +13,12 @@ var hour = 0;
 var mytime = document.querySelector("#mytime");
 var time;
 
-var moves=0;
+var moves = 0;
 var movesElement = document.querySelector('.moves');
 movesElement.innerHTML = "Total Moves: " + moves;
 
 var icons = document.querySelectorAll(".fa-star");
 //var iconsList = document.querySelectorAll(".icons li");
-
-
-
 
 var solutionArray = tileImages.concat(tileImages);
 document.getElementById("restart").addEventListener("click", startGame);
@@ -31,30 +28,28 @@ fliparray = new Array();
 startGame();
 
 function startGame() {
-  clearTimeout(cards);
-  shuffleArray(solutionArray);
+    clearTimeout(cards);
+    shuffleArray(solutionArray);
     gameboard.innerHTML = "";
     for (var i = 0; i <= ((solutionArray.length) - 1); i++) {
-    gameboard.innerHTML += '<div class="col-md-3 col-xs-4 gametile"><img id="cardz' + i + '" src="img/back.jpg" onclick="pickCard(\'' + solutionArray[i] + '\',\'' + i + '\',this);return false;" class="flipimage"></div>';
+        gameboard.innerHTML += '<div class="col-md-3 col-xs-4 gametile"><img id="cardz' + i + '" src="img/back.jpg" onclick="pickCard(\'' + solutionArray[i] + '\',\'' + i + '\',this);return false;" class="flipimage"></div>';
     }
 
     //reset moves
-    moves=0;
+    moves = 0;
     movesElement.innerHTML = "Total Moves: " + moves;
 
-    for (var i= 0; i < icons.length; i++){
+    for (var i = 0; i < icons.length; i++) {
         icons[i].style.visibility = "visible";
     }
 
-
-//restart timer
+    //restart timer
     second = 0;
     minute = 0;
     hour = 0;
     var mytime = document.querySelector("#mytime");
-    mytime.innerHTML =hour+ " hrs "+ minute+" mins "+second+" secs";
+    mytime.innerHTML = hour + " hrs " + minute + " mins " + second + " secs";
     startTimer();
-
 
 }
 
@@ -68,19 +63,18 @@ function pickCard(a, b, c) {
             if (fliparray[0] == fliparray[1]) {
                 console.log('same');
                 matches++; // increment matches when they find one
-                if (matches >= tileImages.length){
+                if (matches >= tileImages.length) {
                     // found all matches
                     // stop timer
-                  stopTimer();
+                    stopTimer();
                     // game over.. update modal and open it from here.
                     endGame();
                     //alert('game over')
                 } else {
                     pickagain();
                 }
-            }
-            else {
-                 cards= setTimeout(hideCard, 1000);
+            } else {
+                cards = setTimeout(hideCard, 1000);
 
             }
             // after 2 cards flipped over, increment moves counter,
@@ -92,33 +86,28 @@ function pickCard(a, b, c) {
 
             iconRate();
 
-  }
+        }
         lastcardpicked = b;
     }
-  }
+}
 
 //star icon rating function
-function iconRate(){
-      //moves;
-      //movesElement.innerHTML = "Total Moves: " + moves;
-    if (moves > 10 && moves < 12){
-          for( i= 0; i < 3; i++){
-              if(i > 1){
-                  icons[i].style.visibility = "hidden";
-              }
-          }
-      }
-      else if (moves > 15){
-          for( i= 0; stars < 3; i++){
-              if(i > 0){
-                  icons[i].style.visibility = "hidden";
-              }
-          }
-      }
-
+function iconRate() {
+    if (moves > 8 && moves < 12) {
+        for (i = 0; i < 3; i++) {
+            if (i > 1) {
+                icons[i].style.visibility = "hidden";
+            }
+        }
+    } else if (moves > 13) {
+        for (i = 0; i < 3; i++) {
+            if (i > 0) {
+                icons[i].style.visibility = "hidden";
+            }
+        }
     }
 
-
+}
 
 function pickagain() {
     cardsflippedover = 0;
@@ -138,33 +127,32 @@ function hideCard() {
     pickagain();
 }
 
-function startTimer(){
-    time = setInterval(function(){
-        mytime.innerHTML =hour+ " hrs "+ minute+" mins "+second+" secs";
+function startTimer() {
+    time = setInterval(function() {
+        mytime.innerHTML = hour + " hrs " + minute + " mins " + second + " secs";
 
         second++;
-        if(second == 60){
+        if (second == 60) {
             minute++;
-            second=0;
+            second = 0;
         }
-        if(minute == 60){
+        if (minute == 60) {
             hour++;
             minute = 0;
         }
-    },1100);
+    }, 1100);
 }
-function stopTimer(){
+
+function stopTimer() {
     clearInterval(time);
 }
 
-//The star rating is not showing the correct rate. The buttons are not refreshing the game.
-function endGame(){
-$("#myModal").modal();
-$('#stats').text(`Total Time:  ${second}  seconds   |   Total Moves:  ${moves}   |   Star Rating: ${i}`);
-//$(".close").bind("click", startGame()); Trying to restart/refresh whole game through the buttons.
+//The star rating is not showing the correct rate. 
+function endGame() {
+    $("#myModal").modal();
+    $('#stats').text(`Total Time:  ${second}  seconds   |   Total Moves:  ${moves}   |   Star Rating: ${i}`);
 
 }
-
 
 function shuffleArray(d) {
     for (var c = d.length - 1; c > 0; c--) {
